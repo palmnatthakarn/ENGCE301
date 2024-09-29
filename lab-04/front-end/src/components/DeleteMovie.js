ee86278bd0067dd15f845b135bdae5ac609d2f38
palmnatthakarn <118332991+palmnatthakarn@users.noreply.github.com>
2024-09-30 01:18:12 +0700

    Add files via upload

---
+++ lab-04/front-end/src/components/DeleteMovie.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+
+const DeleteMovie = ({onChangeForm, deleteMovie }) => {
+
+
+    return(
+        <div className="container">
+            <div className="row">
+                <div className="col-md-7 mrgnbtm">
+                <h2>Delete Movie</h2>
+                <form>
+                    <div className="row">
+                        <div className="form-group col-md-6">
+                            <input type="text" onChange={(e) => onChangeForm(e)} className="form-control" 
+                            name="delete_text" id="delete_text" placeholder="delete_text" />
+                        </div>
+
+                    </div>
+                    <button type="button" onClick={(e) => deleteMovie()} className="btn btn-danger">Delete</button>
+                </form>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default DeleteMovie;
---
+++ lab-04/front-end/src/components/SearchMovie.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const SearchMovie = ({onChangeForm, searchMovie }) => {
+
+    return(
+        <div className="container">
+            <div className="row">
+                <div className="col-md-7 mrgnbtm">
+                <h2>Search Movie</h2>
+                <form>
+                    <div className="row">
+                        <div className="form-group col-md-6">
+                            <input type="text" onChange={(e) => onChangeForm(e)} className="form-control" 
+                            name="search_text" id="search_text" placeholder="search_text" />
+                        </div>
+
+                    </div>
+                    <button type="button" onClick= {(e) => searchMovie()} className="btn btn-danger">Search</button>
+                </form>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+export default SearchMovie
